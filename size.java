50722ec4713483902b207c7df615b60b0db935f5
Sean1016 <111344614+Sean1016@users.noreply.github.com>
2022-09-01 21:05:42 +0900

    Add files via upload

---
+++ size.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class size{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int people_num = Integer.parseInt(br.readLine());
+
+        int people_array[][] = new int[people_num][2];
+        int grade_arr[] = new int[people_num];
+
+        for(int i = 0; i < people_num; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            people_array[i][0] = Integer.parseInt(st.nextToken());
+            people_array[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < people_num; i++){
+            int grade = 1;
+            for(int j = 0; j < people_num; j++){
+                if(people_array[i][0] < people_array[j][0] && people_array[i][1] < people_array[j][1]){
+                    ++grade;
+                }
+            }
+            grade_arr[i] = grade;
+        }
+
+        System.out.println(Arrays.toString(grade_arr));
+        
+    }
+}
