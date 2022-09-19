552ba9948cdb51a0c591a4afc808ec2d864e5390
Sean1016 <111344614+Sean1016@users.noreply.github.com>
2022-09-19 23:36:18 +0900

    Add files via upload

---
+++ jean.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class jean{
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int arr_num = Integer.parseInt(bf.readLine());
+        if( arr_num <= 1000){
+            
+        }
+    }
+}
