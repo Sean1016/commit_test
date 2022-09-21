1e2703bae0015e715d00b6dea009493205b7878d
Sean1016 <111344614+Sean1016@users.noreply.github.com>
2022-09-21 23:32:36 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -1,21 +1,5 @@
 #include <stdio.h>
 
-int main(void) {
-	int i, j, temp;
-	int array[10] = { 1,4,7,2,8,9,10,5,6,3 };
-
-	for (i = 0; i < 10; i++) {
-		for (j = 0; j < 9 - i; j++) {
-			if (array[j] > array[j + 1]) {
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-			}
-		}
-	}
-
-	for (i = 0; i < 10; i++) {
-		printf("%d ", array[i]);
-	}
-	return -1;
+void main() {
+	
 }
