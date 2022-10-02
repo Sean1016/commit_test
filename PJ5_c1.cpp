2da479bf6b152a009da27298b96d9b2771301d47
Sean1016 <111344614+Sean1016@users.noreply.github.com>
2022-10-02 22:51:50 +0900

    Add files via upload

---
+++ PJ5_c1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int a = 10, b = 20, c = 30;
+	int result;
+
+	result = a + b * c;
+	printf("result = %d\n", result);
+
+	result = (a + b) * c;
+	printf("result = %d\n", result);
+
+	result = a < b && c < 0;
+	printf("result = %d\n", result);
+
+	return 0;
+}
